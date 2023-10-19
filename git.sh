bfdd93deb9aa1dd83b168b44592ed71edb45d863
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-19 18:19:54 +0530

    Commit on

--- git.sh
+++ git.sh
@@ -11,12 +11,13 @@ read -p "Enter commit message (or press enter to use default): " commit_message
 # If commit message is empty, use default message with current IST date and time
 if [ -z "$commit_message" ]; then
     commit_message="Commit on $(get_ist_date)"
+else
+    commit_message="$commit_message - $(get_ist_date)"
 fi
 
 # Perform git add and commit
 git add .
 git commit -m "$commit_message"
-git push
 
 echo "Changes committed with message: $commit_message"
 
